ccaeb5ecf30e8d50f1cfce7820545b32492c4d58
Michaela Mueller <michaela.mueller@helmholtz-muenchen.de>
2022-11-28 13:34:52 +0100

    include PC2, PC3 and scree plot

--- workflow/exploration/scripts/pseudobulk.py
+++ workflow/exploration/scripts/pseudobulk.py
@@ -24,7 +24,9 @@ def get_pseudobulks(adata, group_key, agg='mean'):
 
 sc.set_figure_params(dpi=100, frameon=False)
 input_h5ad = snakemake.input.h5ad
-output_png = snakemake.output.png
+output_pca_1_2 = snakemake.output.pca_1_2
+output_pca_2_3 = snakemake.output.pca_2_3
+output_pca_scree = snakemake.output.pca_scree
 bulk_by = snakemake.params['bulk_by']
 color = snakemake.params['color']
 colors = color if isinstance(color, list) else [color]
@@ -59,18 +61,36 @@ sc.pp.log1p(adata_bulk)
 sc.pp.highly_variable_genes(adata_bulk, n_top_genes=2000)
 sc.pp.pca(adata_bulk)
 
+# scree plo
+plt.plot(adata_bulk.uns['pca']['variance_ratio'], marker='.')
+plt.title('Scree plot: PC variance contribution')
+plt.savefig(output_pca_scree)
+
 # remove colors if too many entries
 colors = [c for c in colors if adata_bulk.obs[c].nunique() <= 64]
 
 n_rows = len(colors)
 height = np.max([7, n_rows * 0.2 * 7])
 plt.rcParams['figure.figsize'] = (10, height)
+
 sc.pl.pca(
     adata_bulk[adata_bulk.obs.sample(adata_bulk.n_obs).index],
+    components='1,2',
     color=colors,
-    title=[f'{dataset}: Pseudobulk={bulk_by} color={c}' for c in colors],
+    title=[f'PCA 1-2 {dataset}: Pseudobulk={bulk_by} color={c}' for c in colors],
     show=False,
     ncols=1
 )
 plt.tight_layout()
-plt.savefig(output_png, bbox_inches='tight')
+plt.savefig(output_pca_1_2, bbox_inches='tight')
+
+sc.pl.pca(
+    adata_bulk[adata_bulk.obs.sample(adata_bulk.n_obs).index],
+    components='2,3',
+    color=colors,
+    title=[f'PCA 2-3{dataset}: Pseudobulk={bulk_by} color={c}' for c in colors],
+    show=False,
+    ncols=1
+)
+plt.tight_layout()
+plt.savefig(output_pca_2_3, bbox_inches='tight')
