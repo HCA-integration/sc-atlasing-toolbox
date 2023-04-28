7e74aa06b26422b6d99a0ab4b4650981156131c1
Michaela Mueller <michaela.muller@helmholtz-munich.de>
2023-04-28 16:09:44 -0700

    adapt parameters

--- workflow/common/scripts/embedding.py
+++ workflow/common/scripts/embedding.py
@@ -1,4 +1,3 @@
-import sys
 import numpy as np
 from matplotlib import pyplot as plt
 import scanpy as sc
@@ -9,11 +8,25 @@ params = snakemake.params
 
 if params is None:
     params = {}
+else:
+    params = {k: v for k,v in params.items()}
+
+print('params:', params)
+
+assert 'basis' in params, '"basis" is positional and must be defined'
+basis = params['basis']
 
 adata = sc.read(input_file)
 print(adata)
 
+
+# convert to dense matrix
+try:
+    adata.obsm[basis] = np.array(adata.obsm[basis].todense())
+except:
+    pass
+
 # plot embedding
 sc.set_figure_params(frameon=False, vector_friendly=True, fontsize=9)
-sc.pl.embedding(adata, **params)
+sc.pl.embedding(adata, ncols=1, **params)
 plt.savefig(output_file, bbox_inches='tight', dpi=200)
