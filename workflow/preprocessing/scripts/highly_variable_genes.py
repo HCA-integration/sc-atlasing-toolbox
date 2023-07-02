8c24ac7be43146be59cb6707bd3a41ad825efb3b
Michaela Mueller <michaela.muller@helmholtz-munich.de>
2023-07-01 17:19:31 -0700

    support scenario without HVG selection

--- workflow/preprocessing/scripts/highly_variable_genes.py
+++ workflow/preprocessing/scripts/highly_variable_genes.py
@@ -31,16 +31,20 @@ if adata.n_obs == 0:
 adata.uns["log1p"] = {"base": None}
 sc.pp.filter_genes(adata, min_cells=1)
 
-if lineage_key is not None:
-    logging.info(f'lineage-specific highly variable gene selection using "{lineage_key}"')
-    if batch_key in adata.obs.columns:
-        adata.obs['hvg_batch'] = adata.obs[batch_key].astype(str) + '_' + adata.obs[lineage_key].astype(str)
-    else:
-        adata.obs['hvg_batch'] = adata.obs[lineage_key]
-    batch_key = 'hvg_batch'
+if args is False:
+    logging.info('No highly variable gene parameters provided, including all genes...')
+    adata.var['highly_variable'] = True
+else:
+    if lineage_key is not None:
+        logging.info(f'lineage-specific highly variable gene selection using "{lineage_key}"')
+        if batch_key in adata.obs.columns:
+            adata.obs['hvg_batch'] = adata.obs[batch_key].astype(str) + '_' + adata.obs[lineage_key].astype(str)
+        else:
+            adata.obs['hvg_batch'] = adata.obs[lineage_key]
+        batch_key = 'hvg_batch'
 
-logging.info('Select features...')
-sc.pp.highly_variable_genes(adata, batch_key=batch_key, **args)
+    logging.info('Select features...')
+    sc.pp.highly_variable_genes(adata, batch_key=batch_key, **args)
 
 # add metadata
 if 'preprocessing' not in adata.uns:
--- workflow/preprocessing/test/run_assertions.py
+++ workflow/preprocessing/test/run_assertions.py
@@ -4,7 +4,6 @@ import yaml
 from pprint import pprint
 
 config = yaml.safe_load(open('test/config.yaml', 'r'))
-pprint(config['DATASETS'])
 
 single_outputs = glob.glob('test/out/preprocessing/*/preprocessed.h5ad')
 print(single_outputs)
@@ -12,25 +11,30 @@ print(single_outputs)
 for file in single_outputs:
     adata = sc.read(file)
     dataset = [x for x in config['DATASETS'] if x in file][0]
-    preprocessing_config = config['DATASETS'][dataset]['preprocessing']['assemble']
-    
-    try:
-        if 'counts' in preprocessing_config:
-            assert 'counts' in adata.layers
-        if 'normcounts' in preprocessing_config:
-            assert 'normcounts' in adata.layers
-        if 'highly_variable_genes' in preprocessing_config:
-            assert 'highly_variable' in adata.var.columns
-            assert 'means' in adata.var.columns
-            assert 'dispersions' in adata.var.columns
-            assert 'dispersions_norm' in adata.var.columns
-        if 'pca' in preprocessing_config:
-            assert 'X_pca' in adata.obsm
-        if 'neighbors' in preprocessing_config:
-            assert 'neighbors' in adata.uns
-            assert 'distances' in adata.obsp
-            assert 'connectivities' in adata.obsp
-    except AssertionError as e:
-        print(dataset)
-        pprint(preprocessing_config)
-        raise e
+    preprocessing_config = config['DATASETS'][dataset]['preprocessing']
+
+    print(dataset)
+    print(file)
+    pprint(preprocessing_config)
+
+    if 'counts' in preprocessing_config['assemble']:
+        assert 'counts' in adata.layers
+
+    if 'normcounts' in preprocessing_config['assemble']:
+        assert 'normcounts' in adata.layers
+
+    if 'highly_variable_genes' in preprocessing_config['assemble']:
+        assert 'highly_variable' in adata.var.columns
+        assert 'means' in adata.var.columns
+        assert 'dispersions' in adata.var.columns
+        assert 'dispersions_norm' in adata.var.columns
+        if preprocessing_config['highly_variable_genes'] is False:
+            assert adata.var['highly_variable'].sum() == adata.n_vars
+
+    if 'pca' in preprocessing_config['assemble']:
+        assert 'X_pca' in adata.obsm
+
+    if 'neighbors' in preprocessing_config['assemble']:
+        assert 'neighbors' in adata.uns
+        assert 'distances' in adata.obsp
+        assert 'connectivities' in adata.obsp
