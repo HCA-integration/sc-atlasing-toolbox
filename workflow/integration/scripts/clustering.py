49f91c9901ef439a90d3aeb1c8769d213af2c2c6
Michaela Mueller <michaela.muller@helmholtz-munich.de>
2023-08-01 09:18:28 -0700

    compute neighbors by output type + clustering by output type

--- workflow/integration/scripts/clustering.py
+++ workflow/integration/scripts/clustering.py
@@ -8,6 +8,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 from methods.utils import read_anndata
+from metrics.utils import compute_neighbors, get_from_adata
 
 
 input_file = snakemake.input.h5ad
@@ -16,10 +17,19 @@ resolution = float(snakemake.wildcards.resolution)
 
 logging.info(f'Read anndata file {input_file}...')
 adata = read_anndata(input_file)
+meta = get_from_adata(adata)
 
-logging.info(f'Leiden clustering with resolution {resolution}...')
-cluster_key = f'leiden_{resolution}'
-sc.tl.leiden(adata, resolution=resolution, key_added=cluster_key)
+logging.info('Compute neighbors...')
+for output_type in meta['output_types']:
+    logging.info(f'Computing neighbors for output type {output_type}...')
+    # compute neighbors by output type
+    compute_neighbors(adata, output_type)
+    adata.obsp[f'connectivities_{output_type}'] = adata.obsp['connectivities']
+    adata.obsp[f'distances_{output_type}'] = adata.obsp['distances']
+
+    logging.info(f'Leiden clustering with resolution {resolution}...')
+    cluster_key = f'leiden_{resolution}_{output_type}'
+    sc.tl.leiden(adata, resolution=resolution, key_added=cluster_key)
 
 logging.info('Write file...')
 adata.obs[cluster_key].to_csv(output_dir, sep='\t', index=True)
