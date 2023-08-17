6984c510c81b6d5873b91ba7840ec4c6ed5afb2e
Michaela Mueller <michaela.muller@helmholtz-munich.de>
2023-08-17 15:36:14 -0700

    update relabel script to use backed mode for h5ad or partial reading for zarr, output to linked zarr

--- workflow/relabel/test/assertions.py
+++ workflow/relabel/test/assertions.py
@@ -1,15 +1,30 @@
-import scanpy as sc
+from anndata.experimental import read_elem
+import zarr
 import glob
+import yaml
+import pandas as pd
 
-files = glob.glob('test/out/label_harmonization/**/*.h5ad')
-if len(files) == 0:
+
+files = glob.glob('test/out/relabel/*.zarr')
+if not files:
     print('No files found to test')
 
-for file in files:
-    print(f'read {file}...')
-    adata = sc.read(file)
+config = yaml.safe_load(open('test/config.yaml', 'r'))
 
-    for label in ['cell_type', 'harmonized_label', 'lineage']:
-        print(adata.obs[label].dtype)
-        print(adata.obs[label].value_counts())
+for file in files:
+    dataset = [x for x in config['DATASETS'] if x in file][0]
+    mapping_config = config['DATASETS'][dataset]['relabel']['mapping']
+
+    print(f"read {mapping_config['file']}...")
+    mapping_df = pd.read_table(mapping_config['file'])
+
+    print(f'read {file}...')
+    z = zarr.open(file)
+    obs = read_elem(z["obs"])
+
+    for label in mapping_config['order']:
+        assert label in obs.columns
+        assert label in mapping_df.columns
+        print(obs[label].dtype)
+        print(obs[label].value_counts())
     
