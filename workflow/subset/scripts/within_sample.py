80fa987493fe0b59b38b83f38fd2c2b15b78290a
Michaela Mueller <michaela.muller@helmholtz-munich.de>
2023-08-24 08:32:09 -0700

    add logging statements

--- workflow/subset/scripts/within_sample.py
+++ workflow/subset/scripts/within_sample.py
@@ -1,3 +1,5 @@
+import logging
+logging.basicConfig(level=logging.INFO)
 import numpy as np
 import scanpy as sc
 
@@ -9,20 +11,22 @@ strategy = snakemake.wildcards.strategy
 n_cell_max = snakemake.params.n_cells
 sample_key = snakemake.params.sample_key
 
+
+logging.info(f'Read {input_file}...')
 adata = read_anndata(input_file)
-print(f'Shape before filtering: {adata.shape}')
+logging.info(f'Shape before filtering: {adata.shape}')
 
 try:
     adata.obs[sample_key]
 except KeyError:
-    print(adata)
+    logging.info(adata.__str__())
     raise AssertionError(f'sample key "{sample_key}" not in adata')
 
 adatas = []
 n_samples = adata.obs[sample_key].nunique()
 n_cells_per_sample = int(n_cell_max / n_samples)
 
-print(f'Subset to {n_cells_per_sample} cells per sample...')
+logging.info(f'Subset to {n_cells_per_sample} cells per sample...')
 
 for sample in adata.obs[sample_key].unique():
     ad = adata[adata.obs[sample_key] == sample]
@@ -31,8 +35,9 @@ for sample in adata.obs[sample_key].unique():
 
 adata = sc.concat(adatas)
 adata.uns['subset'] = strategy
-print(f'Subset to {adata.n_obs} cells, {n_samples} samples')
-print(adata)
+logging.info(f'Subset to {adata.n_obs} cells, {n_samples} samples')
+logging.info(adata.__str__())
 
 # save
+logging.info('Write...')
 adata.write(output_file)
