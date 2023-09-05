f1c1ee1a115dcfc70c2adef336a87c62a714b2cd
Michaela Mueller <michaela.muller@helmholtz-munich.de>
2023-09-05 16:43:54 -0700

    use partial read for summary stats

--- workflow/exploration/scripts/summary_stats.py
+++ workflow/exploration/scripts/summary_stats.py
@@ -1,6 +1,10 @@
-import anndata
 import pandas as pd
 from matplotlib import pyplot as plt
+from anndata.experimental import read_elem
+import zarr
+import logging
+logging.basicConfig(level=logging.INFO)
+
 
 input_zarr = snakemake.input.zarr
 output = snakemake.output.tsv
@@ -9,28 +13,32 @@ output_donor = snakemake.output.donor
 study = snakemake.wildcards.study
 
 
-print(f'Read {input_zarr}...')
-adata = anndata.read_zarr(input_zarr)
+logging.info(f'Read {input_zarr}...')
+z = zarr.open(input_zarr)
+obs = read_elem(z['obs'])
+var = read_elem(z['var'])
+n_obs = obs.shape[0]
+n_vars = var.shape[0]
 
-obs_per_sample = adata.obs['sample'].value_counts()
-if adata.n_obs > 0:
+obs_per_sample = obs['sample'].value_counts()
+if n_obs > 0:
     obs_per_sample.plot.barh(figsize=(10,8))
 plt.savefig(output_sample)
 
-obs_per_donor = adata.obs['donor'].value_counts()
-if adata.n_obs > 0:
+obs_per_donor = obs['donor'].value_counts()
+if n_obs > 0:
     obs_per_donor.plot.barh(figsize=(10,8))
 plt.savefig(output_donor)
 
 stats = {
     'study': study,
-    'n_cells': [adata.n_obs],
-    'n_genes': [adata.n_vars],
+    'n_cells': [n_obs],
+    'n_genes': [n_vars],
     'n_samples': [obs_per_sample.shape[0]],
     'n_donors': [obs_per_donor.shape[0]],
     'median_cells_per_sample': [obs_per_sample.median()],
     'median_cells_per_donor': [obs_per_donor.median()],
-    'disease_states': [','.join(adata.obs['disease'].value_counts().index.tolist())],
+    'disease_states': [','.join(obs['disease'].value_counts().index.tolist())],
 }
 
 pd.DataFrame(stats).to_csv(output, sep='\t', index=False)
