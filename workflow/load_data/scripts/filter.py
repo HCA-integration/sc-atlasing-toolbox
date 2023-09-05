8ad1a8a2ce48304bc1e0ca26a04cb9e0b0f82a00
Michaela Mueller <michaela.muller@helmholtz-munich.de>
2023-09-05 15:46:14 -0700

    optimise filter storage: link to input if no cells filtered out

--- workflow/load_data/scripts/filter.py
+++ workflow/load_data/scripts/filter.py
@@ -1,18 +1,59 @@
 """
 Data filtering
 """
-from pprint import pprint
+from pprint import pformat
 import numpy as np
 import scanpy as sc
 import anndata
+import zarr
+import logging
+logging.basicConfig(level=logging.INFO)
+
+from utils_pipeline.io import link_zarr
+
+
+def save_empty(input_zarr, output_zarr, output_removed):
+    # link unfiltered
+    link_zarr(
+        in_dir=input_zarr,
+        out_dir=output_zarr,
+        relative_path=False,
+    )
+
+    # save removed cells
+    z = zarr.open(input_zarr)
+    obs = anndata.experimental.read_elem(z['obs'])
+    var = anndata.experimental.read_elem(z['var'])
+    uns = anndata.experimental.read_elem(z['uns'])
+    adata_empty = anndata.AnnData(
+        X=np.zeros(shape=(0, var.shape[0])),
+        obs=obs[:0],
+        var=var,
+        uns=uns
+    )
+    adata_empty.write_zarr(output_removed)
+    # link_zarr(
+    #     in_dir=input_zarr,
+    #     out_dir=output_removed,
+    #     file_names=['uns'],
+    #     overwrite=True,
+    #     relative_path=False,
+    # )
+
 
 input_zarr = snakemake.input.zarr
 output_zarr = snakemake.output.zarr
 output_removed = snakemake.output.removed
-params = snakemake.params['filter']
+params = snakemake.params.get('filter', {})
+
+logging.info(pformat(params))
+
+if len(params) == 0:
+    save_empty(input_zarr, output_zarr, output_removed)
+    exit(0)
 
 adata = anndata.read_zarr(input_zarr)
-print(adata)
+logging.info(adata.__str__())
 
 # subset to available annotations TODO: make this more generic
 if 'cell_type' in adata.obs.columns:
@@ -20,9 +61,9 @@ if 'cell_type' in adata.obs.columns:
 
 # explicit filter
 if 'remove_by_colum' in params:
-    print('remove by columns...')
+    logging.info('remove by columns...')
     ex_filters = params['remove_by_colum']
-    pprint(ex_filters)
+    logging.info(pformat(ex_filters))
     for column, values in ex_filters.items():
         adata_filtered = adata_filtered[~adata_filtered.obs[column].isin(values)]
 
@@ -30,7 +71,7 @@ if 'remove_by_colum' in params:
 # mito filter
 if 'mito_pct' in params.keys():
     mito_threshold = params['mito_pct']
-    print(f'remove by mitochondrial percentage of {mito_threshold}%...')
+    logging.info(f'remove by mitochondrial percentage of {mito_threshold}%...')
     adata_filtered.var["mito"] = adata_filtered.var['feature_name'].str.startswith("MT-")
     sc.pp.calculate_qc_metrics(adata_filtered, qc_vars=["mito"], inplace=True)
     adata_filtered = adata_filtered[adata_filtered.obs['pct_counts_mito'] < mito_threshold]
@@ -41,17 +82,21 @@ if 'cells_per_sample' in params.keys():
     max_cells = params['cells_per_sample']['max']
     min_cells = min_cells if isinstance(min_cells, int) else 0
     max_cells = max_cells if isinstance(max_cells, int) else np.inf
-    print(f'remove by min={min_cells}, max={max_cells} cells per sample...')
+    logging.info(f'remove by min={min_cells}, max={max_cells} cells per sample...')
     n_cells_per_sample = adata_filtered.obs['sample'].value_counts()
     samples_to_keep = n_cells_per_sample[n_cells_per_sample.between(min_cells, max_cells)].index
     adata_filtered = adata_filtered[adata_filtered.obs['sample'].isin(samples_to_keep)]
 
-print('Save filtered...')
-print(adata_filtered)
+adata_removed = adata.n_obs - sum(adata.obs_names.isin(adata_filtered.obs_names))
+logging.info(f'removed {adata_removed} cells')
+
+if adata_removed == 0:
+    save_empty(input_zarr, output_zarr, output_removed)
+    exit(0)
+
+logging.info('Save filtered...')
+logging.info(adata_filtered.__str__())
 adata_filtered.write_zarr(output_zarr)
 
-adata_removed = adata.n_obs - sum(adata.obs_names.isin(adata_filtered.obs_names))
-print(f'removed {adata_removed} cells')
-
-print('Save removed...')
+logging.info('Save removed...')
 adata[~adata.obs_names.isin(adata_filtered.obs_names)].write_zarr(output_removed)
