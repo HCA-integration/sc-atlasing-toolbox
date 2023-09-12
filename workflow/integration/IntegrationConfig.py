08342f052259967a811914e785445af508e245e8
Michaela Mueller <michaela.muller@helmholtz-munich.de>
2023-09-12 01:26:42 -0700

    add ModuleConfig class and IntegrationConfig class

---
+++ workflow/integration/IntegrationConfig.py
@@ -0,0 +1,94 @@
+import numpy as np
+import pandas as pd
+from snakemake.utils import Paramspace
+
+from utils.ModuleConfig import ModuleConfig
+from utils.config import _get_or_default_from_config
+from utils.misc import expand_dict_and_serialize, unique_dataframe
+
+
+class IntegrationConfig(ModuleConfig):
+    
+    def __init__(self, module_name: str, config: dict, parameters: pd.DataFrame = None):
+        super().__init__(module_name, config, parameters)
+        self.wildcard_names = ['dataset', 'file_id', 'batch', 'label', 'method', 'hyperparams']
+        
+        # remove redundant label wildcards
+        if 'use_cell_type' in self.wildcards_df.columns:
+            self.wildcards_df['label'] = np.where(
+                self.wildcards_df['use_cell_type'],
+                self.wildcards_df['label'],
+                'None'
+            )
+            self.wildcards_df = unique_dataframe(self.wildcards_df)
+
+        self.paramspace = Paramspace(
+            self.wildcards_df[self.wildcard_names],
+            filename_params=['method', 'hyperparams'],
+            filename_sep='--',
+        )
+    
+    
+    def set_wildcards(
+        self,
+        config_params: list = None,
+        wildcard_names: list = None,
+        explode_by: list = None,
+        config_keys: list = None,
+        warn: bool = False,
+    ):
+        # parameters = pd.read_table(workflow.source_path('params.tsv'))
+        # parameters['output_type'] = parameters['output_type'].str.split(',')
+        self.set_hyperparams()
+        super().set_wildcards(
+            config_params=['methods', 'label', 'batch', 'norm_counts', 'raw_counts'],
+            wildcard_names=['method', 'label', 'batch', 'norm_counts', 'raw_counts'],
+            explode_by=['method', 'batch'],
+        )#.merge(parameters,on='method')
+        self.wildcards_df = self.wildcards_df.merge(
+            self.hyperparams_df,
+            on=['dataset', 'method'],
+            how='left'
+        )
+
+
+    def set_hyperparams(self):
+        """
+        Get hyperparameters specific to each method of a module for all datasets
+
+        :param config: config containing dataset specific information
+        :param module: name of module, key must be present for each dataset entry
+        """
+        records = []
+        for dataset, dataset_dict in self.datasets.items():
+            methods_config = _get_or_default_from_config(
+                config=dataset_dict,
+                defaults=self.config['defaults'][self.module_name],
+                key=self.module_name,
+                value='methods',
+                update=False,
+            )
+            if methods_config is None:
+                continue
+            for method, hyperparams_dict in methods_config.items():
+                if isinstance(hyperparams_dict, dict):
+                    records.extend(
+                        (dataset, method, *rec)
+                        for rec in expand_dict_and_serialize(hyperparams_dict)
+                    )
+                else:
+                    records.append((dataset, method, str(hyperparams_dict), hyperparams_dict))
+        self.hyperparams_df = pd.DataFrame(
+            records,
+            columns=['dataset', 'method', 'hyperparams', 'hyperparams_dict']
+        )
+        
+        # write to file 
+        unique_dataframe(
+            self.hyperparams_df[['method', 'hyperparams', 'hyperparams_dict']]
+        ).to_csv(self.out_dir / 'hyperparams.tsv', sep='\t', index=False)
+
+
+
+    def get_hyperparams(self):
+        return self.hyperparams_df
---
+++ workflow/utils/ModuleConfig.py
@@ -0,0 +1,262 @@
+from typing import Union
+from pathlib import Path
+import pandas as pd
+import hashlib
+from snakemake.io import expand
+from snakemake.utils import Paramspace
+
+from .config import _get_or_default_from_config
+from .misc import unique_dataframe
+
+
+class ModuleConfig:
+
+    module_name = ''
+    config = {}
+    datasets = {}
+    wildcard_names = []
+    input_files = {}
+    input_file_wildcards = dict(dataset=[], file_id=[], file_name=[])
+    wildcards_df = pd.DataFrame()
+    parameters_df = pd.DataFrame()
+    paramspace = pd.DataFrame()
+    out_dir = Path()
+    image_dir = Path()
+
+
+    def __init__(self, module_name: str, config: dict, parameters: pd.DataFrame = None):
+        self.module_name = module_name
+        self.config = config
+        self.set_defaults()
+        
+        self.out_dir = Path(self.config['output_dir']) / self.module_name
+        self.out_dir.mkdir(parents=True, exist_ok=True)
+        self.image_dir = Path(self.config['images']) / self.module_name
+        
+        # get subset of datasets for this module
+        all_datasets = config['DATASETS']
+        default_datasets = config['defaults'].get('datasets', all_datasets.keys())
+        self.datasets = {
+            dataset: entry for dataset, entry in all_datasets.items()
+            if dataset in default_datasets
+            and module_name in entry.get('input', {}).keys()
+        }
+        
+        for dataset in self.datasets:
+            self.set_defaults_per_dataset(dataset)
+            self.set_input_file_per_dataset(dataset)
+
+
+        # determine wildcards
+        self.set_wildcards()
+        self.wildcard_names = ['dataset']
+        
+        # get parameters
+        if parameters is not None:
+            self.parameters_df = parameters
+            self.wildcards_df = self.wildcards_df.merge(self.parameters_df, how='left')
+        
+        # add input file wildcards
+        self.wildcards_df = self.wildcards_df.merge(
+            pd.DataFrame(self.input_file_wildcards, dtype='object'),
+            on='dataset',
+            how='left',
+        )
+        
+        self.paramspace = Paramspace(self.wildcards_df[self.wildcard_names])
+
+
+    def set_defaults(self, warn: bool = False):
+        self.config['DATASETS'] = self.config.get('DATASETS', {})
+        self.config['defaults'] = self.config.get('defaults', {})
+        self.config['defaults'][self.module_name] = self.config['defaults'].get(self.module_name, {})
+        datasets = list(self.config['DATASETS'].keys())
+        self.config['defaults']['datasets'] = self.config['defaults'].get('datasets', datasets)
+
+
+    def set_defaults_per_dataset(self, dataset: str, warn: bool = False):
+        # update entries for each dataset
+        entry = _get_or_default_from_config(
+            config=self.config['DATASETS'],
+            defaults=self.config['defaults'],
+            key=dataset,
+            value=self.module_name,
+            return_missing={},
+            warn=warn,
+            update=True,
+        )
+        
+        # for TSV input make sure integration methods have the proper types TODO: deprecate
+        if self.module_name == 'integration' and isinstance(entry, list):
+            entry = {k: self.config['defaults'][self.module_name][k] for k in entry}
+        
+        # set entry in config
+        self.config['DATASETS'][dataset][self.module_name] = entry
+        self.datasets[dataset][self.module_name] = entry
+
+
+    def set_input_file_per_dataset(self, dataset, digest_size=5):
+        """Get input files for a given module and dataset
+        This function maps an input file with its unique identifier, if no file ID is specified
+        
+        :param dataset: dataset key in config['DATASETS']
+        """
+        input_files = self.get_for_dataset(dataset, query=['input', self.module_name])
+        
+        if isinstance(input_files, str):
+            input_files = [input_files]
+        
+        if isinstance(input_files, list):
+            input_files = {
+                hashlib.blake2b(file.encode('utf-8'), digest_size=digest_size).hexdigest(): file
+                for file in input_files
+            }
+        
+        if not isinstance(input_files, dict):
+            raise ValueError(f'input_files must be a list or dict, but is {type(input_files)}')
+        
+        self.input_files[dataset] = input_files
+        
+        # Reshape the file ID to dataset mapping to a lists of wildcards
+        for file_id, file in input_files.items():
+            self.input_file_wildcards['dataset'].append(dataset)
+            self.input_file_wildcards['file_id'].append(file_id)
+            self.input_file_wildcards['file_name'].append(file)
+        
+        # write to file
+        pd.DataFrame(self.input_files).to_csv(
+            self.out_dir / 'input_files.tsv',
+            sep='\t',
+            index=False
+        )
+
+
+    def get_for_dataset(
+        self,
+        dataset: str,
+        query: list,
+        default: Union[str,bool,float,int,dict,list, None] = None,
+        warn: bool = False,
+    ) -> Union[str,bool,float,int,dict,list, None]:
+        """Get any key from the config via query
+
+        Args:
+            dataset (str): dataset key in config['DATASETS']
+            query (list): list of keys to walk down the config
+            default (Union[str,bool,float,int,dict,list, None], optional): default value if key not found. Defaults to None.
+
+        Returns:
+            Union[str,bool,float,int,dict,list, None]: value of query in config
+        """
+        # start at top level
+        value = self.config['DATASETS'][dataset]
+
+        # walk down query
+        for q in query:
+            if warn and q not in value:
+                warnings.warn(f'key {q} not found in config for query {query}, returning default')
+            value = value.get(q, default)
+        return value
+
+
+    def set_wildcards(
+        self,
+        config_params: list = None,
+        wildcard_names: list = None,
+        explode_by: list = None,
+        config_keys: list = None,
+        warn: bool = False,
+    ):
+        """
+        Collect wildcards and parameters from an configuration instance (e.g. dataset) for a given module.
+        This function assumes that the keys of the given config keys are the different instances that contain specific parameters for different modules.
+
+        :param config_params: List of parameters for each config entry of a module
+            e.g. ['integration', 'label', 'batch']
+        :param wildcard_names: names of wildcards to be extracted.
+            Must map to config keys, and prepended by a wildcard name for the config entries
+            e.g. ['dataset', 'method', 'label', 'batch']
+        :param explode_by: column to explode by, expecting list entry for that column
+        :param config_keys: list of entries to subset the config by, otherwise use all keys
+        """
+        if len(self.datasets) == 0 or not config_params:
+            config_params = []
+        
+        if not wildcard_names:
+            wildcard_names = config_params
+        
+        if len(config_params) != len(wildcard_names):
+            raise ValueError('config_params and wildcard_names must be of same length')
+            
+        if config_keys is None:
+            config_keys = self.datasets.keys()
+
+        # collect entries for dataframe
+        records = [
+            (
+                key,
+                *[
+                    _get_or_default_from_config(
+                        config=self.datasets[key],
+                        defaults=self.config['defaults'].get(self.module_name),
+                        key=self.module_name,
+                        value=param,
+                        update=True,
+                        warn=warn,
+                    )
+                    for param in config_params
+                ]
+            )
+            for key in config_keys
+        ]
+        df = pd.DataFrame.from_records(records, columns=[*['dataset']+wildcard_names])
+        if explode_by is not None:
+            explode_by = [explode_by] if isinstance(explode_by, str) else explode_by
+            for column in explode_by:
+                df = df.explode(column)
+        self.wildcards_df = df.reset_index(drop=True)
+
+
+    def get_config(self):
+        return self.config
+
+
+    def get_datasets(self):
+        return self.datasets
+
+
+    def get_wildcards(self, exclude: list = None) -> dict:
+        if exclude is None:
+            exclude = []
+        wildcard_names = [wildcard for wildcard in self.wildcard_names if wildcard not in exclude]
+        return unique_dataframe(self.wildcards_df[wildcard_names]).to_dict('list')
+
+
+    def get_output_files(self, pattern, exclude: list = None):
+        if exclude is None:
+            exclude = []
+        return expand(pattern, zip, **self.get_wildcards(exclude=exclude))
+
+
+    def get_input_file_wildcards(self):
+        return self.input_file_wildcards
+
+
+    def get_input_files(self):
+        return self.input_files
+
+
+    def get_input_files_per_dataset(self, dataset):
+        return self.input_files[dataset]
+
+
+    def get_input_file(self, dataset, file_id):
+        return get_input_files_per_dataset(dataset)[file_id]
+
+
+    def get_parameters(self):
+        return self.parameters_df
+
+
+    def get_paramspace(self):
+        return self.paramspace
