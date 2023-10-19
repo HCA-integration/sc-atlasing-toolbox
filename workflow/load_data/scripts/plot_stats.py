5f0bc82e738076149a034d2cfd1e465bc3ac8686
Michaela Mueller <michaela.muller@helmholtz-munich.de>
2023-10-19 06:59:07 -0700

    more verbose plot

--- workflow/load_data/scripts/plot_stats.py
+++ workflow/load_data/scripts/plot_stats.py
@@ -11,9 +11,12 @@ series = [
     for study, f in in_stats.items()
 ]
 stats_df = pd.concat(series, axis=0)
-print(stats_df)
+stats_df['intersection_fraction'] = stats_df['intersection_fraction'].astype(float).round(2)
 
-stats_df['intersection_fraction'].plot.barh(
+plot = stats_df['intersection_fraction'].sort_values().plot.barh(
     title='Intersection of donor_id with best matching DCP ID'
 )
+for container in plot.containers:
+    plot.bar_label(container)
+plot.set_xlabel('ID overlap fraction (normed over CxG donors/samples)')
 plt.savefig(out_intersection, bbox_inches='tight')
