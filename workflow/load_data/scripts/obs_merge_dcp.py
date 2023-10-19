6a975016a2a2947d3df8659a61cc3fd9c2bfc387
Michaela Mueller <michaela.muller@helmholtz-munich.de>
2023-10-19 06:58:48 -0700

    allow to match by sample as well as donor

--- workflow/load_data/scripts/obs_merge_dcp.py
+++ workflow/load_data/scripts/obs_merge_dcp.py
@@ -16,38 +16,51 @@ def explode_table(df, col, sep=' \|\| '):
 
 id_cols = snakemake.params.id_cols
 
-obs_df = read_elem(zarr.open(in_file)['obs'])
 dcp_tsv = pd.read_table(in_dcp)
-
-obs_ids = set(obs_df['donor_id'].unique())
-n_donors = len(obs_ids)
+with zarr.open(in_file) as z:
+    obs_df = read_elem(z['obs'])
 
 # identify ID column
 cols = []
 intersection = []
+n_donors = []
+cxg_ids = []
 
 for col in id_cols:
     if col not in dcp_tsv.columns:
         continue
     
-    # explode ID column
-    dcp_tsv_exploded = explode_table(dcp_tsv, col)
+    for cxg_id in ['donor_id', 'sample']:
+        # explode ID column
+        dcp_tsv_exploded = explode_table(dcp_tsv, col)
 
-    # count ID overlaps
-    dcp_ids = set(dcp_tsv_exploded[col].unique())
-    intersect = obs_ids.intersection(dcp_ids)
-    n_intersect = len(intersect)
+        # count ID overlaps
+        dcp_ids = set(dcp_tsv_exploded[col].unique())
+        obs_ids = set(obs_df[cxg_id].unique())
+        intersect = obs_ids.intersection(dcp_ids)
 
-    cols.append(col)
-    intersection.append(n_intersect)
+        cols.append(col)
+        cxg_ids.append(cxg_id)
+        intersection.append(len(intersect))
+        n_donors.append(len(obs_ids))
 
-intersect_df = pd.DataFrame({'columns': cols, 'intersection': intersection}).set_index('columns')
-intersect_df['cxg_donors'] = n_donors
-intersect_df['intersection_fraction'] = intersect_df['intersection'] / n_donors
+intersect_df = pd.DataFrame(
+    {
+        'dcp_column': cols,
+        'cxg_column': cxg_ids,
+        'intersection': intersection,
+        'n_cxg': n_donors,
+    }
+)
+intersect_df['intersection_fraction'] = intersect_df['intersection'] / intersect_df['n_cxg']
 print(intersect_df)
 
+# identify ID column with most overlap
 argmax = intersect_df['intersection'].argmax()
-id_col = intersect_df.index.tolist()[argmax]
+intersect_max = intersect_df.iloc[argmax,:]
+assert isinstance(intersect_max, pd.Series), f'max intersection is not a Series\n{intersect_max}'
+id_col = intersect_max['dcp_column']
+cxg_col = intersect_max['cxg_column']
 
 metadata_columns  = [id_col] + snakemake.params.metadata_cols
 metadata_columns = [c for c in metadata_columns if c in dcp_tsv.columns]
@@ -60,7 +73,7 @@ dcp_tsv = dcp_tsv[metadata_columns].drop_duplicates()
 print(dcp_tsv)
 obs_df = obs_df.merge(
     dcp_tsv,
-    left_on='donor_id',
+    left_on=cxg_col,
     right_on=id_col,
     how='left'
 )
@@ -73,6 +86,6 @@ print(obs_df)
 obs_df.to_csv(out_obs, sep='\t', index=False)
 
 # save stats
-intersect_df.loc[id_col,:][['intersection', 'cxg_donors', 'intersection_fraction']].to_csv(out_stats, sep='\t', index=True)
+intersect_max[['dcp_column', 'cxg_column', 'intersection', 'n_cxg', 'intersection_fraction']].to_csv(out_stats, sep='\t', index=True)
 # TODO: metadata column completeness
 # TODO: aggregatedness of donor ID
