00f9f14d63189ef3d2dd3bec77d2dbdb850a791f
Michaela Mueller <michaela.muller@helmholtz-munich.de>
2023-11-02 10:15:13 -0700

    log to error

--- workflow/batch_analysis/scripts/batch_pcr.py
+++ workflow/batch_analysis/scripts/batch_pcr.py
@@ -12,11 +12,10 @@ try:
     from sklearnex import patch_sklearn
     patch_sklearn()
 except ImportError:
-    logger.info('no hardware acceleration for sklearn')
+    logger.error('no hardware acceleration for sklearn')
 
 import logging
 logger = logging.getLogger('Batch PCR')
-logger.setLevel(logging.INFO)
 
 input_file = snakemake.input.anndata
 setup_file = snakemake.input.setup
@@ -25,7 +24,7 @@ covariate = snakemake.wildcards.covariate
 sample_key = snakemake.params.get('sample_key')
 n_threads = snakemake.threads
 
-logger.info('Read anndata file...')
+logger.error('Read anndata file...')
 z = zarr.open(input_file)
 X_pca = read_elem(z['obsm/X_pca'])
 obs = read_elem(z['obs'])
@@ -42,16 +41,16 @@ if not isinstance(X_pca, np.ndarray):
     X_pca = X_pca.toarray()
 
 
-logger.info('Read covariate setup...')
+logger.error('Read covariate setup...')
 with open(setup_file, 'r') as f:
     setup = yaml.safe_load(f)
 n_permute = setup['n_permute']
 # n_permute = min(snakemake.params.get('n_permute', 0), n_permute)
-logger.info(f'n_permute: {n_permute}')
+logger.error(f'n_permute: {n_permute}')
 
 
 # PCR for permuted covariates
-logger.info(f'Permute covariate: "{covariate}"')
+logger.error(f'Permute covariate: "{covariate}"')
 perm_covariates = []
 for i in range(n_permute):
     covariate_perm = f'{covariate}-{i}'
@@ -70,19 +69,19 @@ for i in range(n_permute):
 # PC regression for all covariates
 adata = AnnData(obs=obs, obsm={'X_pca': X_pca}, uns=uns)
 covariates = [covariate]+perm_covariates
-# adatas = [adata[obs[covariate].notna()] for _ in covariates]
+# adatas = [adata[obs[covariate].notna()] for covariate in covariates]
 pcr_scores = []
 
 
 def compute_pcr(covariate):
-    logger.info(f'PCR for covariate: "{covariate}"')
+    logger.error(f'PCR for covariate: "{covariate}"')
     pcr = scib.me.pcr(
         adata[obs[covariate].notna()],
         covariate=covariate,
         recompute_pca=False,
         verbose=False
     )
-    logger.info(pcr)
+    logger.error(pcr)
     return pcr
 
 
