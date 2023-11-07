284a99eee38f447b672da814baa5e45ff842f341
Ana Maria Cujba <ana.m.cujba@gmail.com>
2023-11-07 16:46:53 +0000

    added_extra_summary_stats

--- workflow/exploration/scripts/summary_stats.py
+++ workflow/exploration/scripts/summary_stats.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import scanpy as sc
 from matplotlib import pyplot as plt
 from anndata.experimental import read_elem
 import zarr
@@ -30,6 +31,9 @@ if n_obs > 0:
     obs_per_donor.plot.barh(figsize=(10,8))
 plt.savefig(output_donor)
 
+adata.var["mito"] = adata.var['feature_name'].str.startswith("MT-")
+sc.pp.calculate_qc_metrics(adata, qc_vars=["mito"], inplace=True)
+
 stats = {
     'study': study,
     'n_cells': [n_obs],
@@ -38,7 +42,10 @@ stats = {
     'n_donors': [obs_per_donor.shape[0]],
     'median_cells_per_sample': [obs_per_sample.median()],
     'median_cells_per_donor': [obs_per_donor.median()],
-    'disease_states': [','.join(obs['disease'].value_counts().index.tolist())],
+    'disease_states': [','.join(obs['disease'].unique().to_list())],
+    'mean_cells_per_sample': [obs['sample'].value_counts().mean()], 
+    'median_counts_per_cell': [obs['total_counts'].median()],
+    'median_genes_per_cell': [obs['n_genes_by_counts'].median()]
 }
 
 pd.DataFrame(stats).to_csv(output, sep='\t', index=False)
