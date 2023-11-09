b620a39b195fc96d88fc187314b943589146870a
Michaela Mueller <michaela.muller@helmholtz-munich.de>
2023-11-09 08:04:29 -0800

    use output_type as wildcard

--- workflow/preprocessing/scripts/neighbors.py
+++ workflow/preprocessing/scripts/neighbors.py
@@ -14,14 +14,15 @@ except ImportError as e:
 
 from utils.io import read_anndata, link_zarr
 from utils.processing import assert_neighbors
+from utils.misc import ensure_dense
 
 
 input_file = snakemake.input[0]
 output_file = snakemake.output[0]
-params = dict(snakemake.params.args.items())
+params = snakemake.params.get('args', {})
 
 logging.info(f'Read {input_file}...')
-adata = read_anndata(input_file, obs='obs', obsm='obsm', uns='uns')
+adata = read_anndata(input_file, obs='obs', obsm='obsm', uns='uns', var='var')
 
 if adata.n_obs == 0:
     logging.info('No data, write empty file...')
@@ -29,20 +30,35 @@ if adata.n_obs == 0:
     exit(0)
 
 # set defaults
-if 'use_rep' not in params:
-    if 'X_pca' in adata.obsm:
-        params['use_rep'] = 'X_pca'
-        params['n_pcs'] = adata.obsm['X_pca'].shape[1]
-    else:
-        params['use_rep'] = 'X'
+if params == False:
+    # use existing kNN graph
+    adata.obsp = read_anndata(input_file, obs='obs', obsp='obsp').obsp
+    assert_neighbors(adata, check_params=False)
+    adata.uns['neighbors']['params'] = adata.uns['neighbors'].get('params', {})
+    adata.uns['neighbors']['params'] |= dict(
+        connectivities_key='connectivities',
+        distances_key='distances',
+        use_rep='X',
+    )
+    assert_neighbors(adata)
+else:
+    if 'use_rep' not in params:
+        if 'X_pca' in adata.obsm:
+            params['use_rep'] = 'X_pca'
+            params['n_pcs'] = adata.obsm['X_pca'].shape[1]
+        else:
+            params['use_rep'] = 'X'
+    if params['use_rep'] == 'X':
         adata.X = read_anndata(input_file, X='X').X
-logging.info(str(params))
+        ensure_dense(adata)
+    logging.info(str(params))
 
-# compute kNN graph
-sc.pp.neighbors(adata, **params)
-assert_neighbors(adata)
+    # compute kNN graph
+    sc.pp.neighbors(adata, **params)
+    assert_neighbors(adata)
 
 logging.info(f'Write to {output_file}...')
+del adata.X
 adata.write_zarr(output_file)
 
 if input_file.endswith('.zarr'):
