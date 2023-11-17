793c86f620073f08996e5fb2a0a7906a90dd137f
Michaela Mueller <michaela.muller@helmholtz-munich.de>
2023-11-17 07:57:36 -0800

    refactor exploration to explicitly take files as input

---
+++ workflow/exploration/scripts/summary_plot.py
@@ -0,0 +1,24 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+
+
+dfs = [pd.read_table(x) for x in snakemake.input.tsv]
+df = pd.concat(dfs).set_index('study')
+df.to_csv(snakemake.output.tsv, sep='\t')
+
+# plot per study
+n_rows = df.shape[0]
+df.sort_values(by='n_cells', ascending=True).plot.barh(
+    subplots=True,
+    sharex=False,
+    sharey=True,
+    layout=(int(df.shape[1] / 2), 2),
+    figsize=(12, np.max([5, 1.2 * n_rows]))
+)
+
+plt.tight_layout()
+plt.savefig(snakemake.output.png)
+
+# save aggregated stats
+df[['n_cells', 'n_samples', 'n_donors']].agg('sum').to_csv(snakemake.output.aggregate, sep='\t')
