2d8e837ee33b6464997a4bdffb149037a2e8e5bf
Michaela Mueller <michaela.muller@helmholtz-munich.de>
2023-12-13 09:08:33 -0800

    add config and functionality for merging existing columns

--- workflow/relabel/test/assertions.py
+++ workflow/relabel/test/assertions.py
@@ -13,18 +13,25 @@ config = yaml.safe_load(open('test/config.yaml', 'r'))
 
 for file in files:
     dataset = [x for x in config['DATASETS'] if x in file][0]
-    mapping_config = config['DATASETS'][dataset]['relabel']['mapping']
-
-    print(f"read {mapping_config['file']}...")
-    mapping_df = pd.read_table(mapping_config['file'])
-
+    
     print(f'read {file}...')
-    z = zarr.open(file)
-    obs = read_elem(z["obs"])
+    with zarr.open(file) as z:
+        obs = read_elem(z["obs"])
+    
+    # new column mapping
+    new_label_cfg = config['DATASETS'][dataset]['relabel'].get('new_columns')
+    mapping_df = pd.read_table(new_label_cfg['file'])
 
-    for label in mapping_config['order']:
+    for label in new_label_cfg['order']:
         assert label in obs.columns
         assert label in mapping_df.columns
         print(obs[label].dtype)
         print(obs[label].value_counts())
     
+    # existing column merge
+    merge_cfg = config['DATASETS'][dataset]['relabel'].get('merge_columns')
+    if merge_cfg is None:
+        continue
+    sep = merge_cfg.get('order')
+    merge_df = pd.read_table(merge_cfg['file'])
+    # TODO: check
