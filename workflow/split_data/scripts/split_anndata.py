6468a3dd0b4d2e1a70103edaf350ee131e5de8c8
Michaela Mueller <michaela.muller@helmholtz-munich.de>
2024-01-25 07:26:10 -0800

    add option to exclude slots and configuration for backed and dask reading

--- workflow/split_data/scripts/split_anndata.py
+++ workflow/split_data/scripts/split_anndata.py
@@ -2,38 +2,46 @@ import sys
 from pathlib import Path
 import numpy as np
 import scanpy as sc
-from scipy import sparse
 import warnings
 warnings.filterwarnings("ignore")
 import logging
 logging.basicConfig(level=logging.INFO)
 import anndata as ad
+from pprint import pformat
+from dask import array as da
 
 from utils.io import read_anndata
 from utils.accessors import adata_to_memory
-# from utils.misc import ensure_sparse
+from utils.misc import apply_layers
+from utils.sparse_dask import sparse_dataset_as_dask
 
 input_file = snakemake.input[0]
 output_dir = snakemake.output[0]
 split_key = snakemake.wildcards.key
 values = snakemake.params.get('values', [])
 backed = snakemake.params.get('backed', False)
+dask = snakemake.params.get('dask', False)
+exclude_slots = snakemake.params.get('exclude_slots', [])
+n_threads = snakemake.threads
 
 out_dir = Path(output_dir)
 if not out_dir.exists():
     out_dir.mkdir()
 
 logging.info(f'Read anndata file {input_file}...')
-adata = read_anndata(input_file, backed=backed)
+adata = read_anndata(
+    input_file,
+    backed=backed,
+    dask=dask,
+    exclude_slots=exclude_slots,
+)
 logging.info(adata.__str__())
-
-# for layer in ['X']+list(adata.layers.keys()):
-#     ensure_sparse(adata, layer=layer)
 
 file_value_map = {
     s.replace(' ', '_').replace('/', '_'): s
     for s in adata.obs[split_key].astype(str).unique()
 }
+logging.info(f'file_value_map: {pformat(file_value_map)}')
 # split_files = list(file_value_map.keys())
 # split_files = set(split_files + values)
 split_files = values
@@ -57,11 +65,19 @@ for split_file in split_files:
             var=adata_sub.var,
             varm=adata_sub.varm,
             varp=adata_sub.varp,
-            layers=adata_sub.layers,
+            layers={
+                layer: np.zeros(adata_sub.shape)
+                for layer in adata_sub.layers
+            },
             uns=adata_sub.uns,
         )
     else:
-        adata_sub = adata_to_memory(adata_sub.copy())
+        logging.info('Copy subset...')
+        adata_sub = apply_layers(
+            adata_sub.copy(),
+            lambda x: x.rechunk({0: 1000}) if isinstance(x, da.Array) else x
+        )
+        adata_sub = adata_to_memory(adata_sub)
     
     # write to file
     logging.info(f'Write to {out_file}...')
