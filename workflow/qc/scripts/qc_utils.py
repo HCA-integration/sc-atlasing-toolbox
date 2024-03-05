171378f5f8a24f45d4b9f48da76c7fc7cde301f3
Michaela Mueller <michaela.muller@helmholtz-munich.de>
2024-03-05 04:49:45 -0800

    parse alternative thresholds

--- workflow/qc/scripts/qc_utils.py
+++ workflow/qc/scripts/qc_utils.py
@@ -31,7 +31,12 @@ def read_threshold_file(file: str):
     prefixes = ['percent_mito', 'n_genes', 'n_counts']
     if all(not col.startswith(prefix) for col in df.columns for prefix in prefixes):
         logging.warning(f'WARNING: None of expected QC stat columns found in {file}.')
-    return df
+    columns = [col for col in df.columns if any(col.startswith(x) for x in prefixes) or col in ['file_id', 'threshold_type']]
+    if 'threshold_type' in columns:
+        df = df[df['threshold_type'].isin(['user', 'alternative'])]
+    else:
+        df['threshold_type'] = 'user'
+    return df[columns].drop_duplicates()
 
 
 def unpack_thresholds(row):
