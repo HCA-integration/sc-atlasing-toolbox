9c9d8f6b8da64665888f7559af77337f9dab12c4
Michaela Mueller <michaela.muller@helmholtz-munich.de>
2024-03-21 03:39:40 -0700

    optimise filter for subset only when cells need to be removed

--- workflow/filter/scripts/filter.py
+++ workflow/filter/scripts/filter.py
@@ -21,51 +21,53 @@ dask = params.get('dask', True)
 subset = params.get('subset', False)
 
 kwargs = {'obs': 'obs'}
-if subset:
-    kwargs |= {
-        'X': 'X',
-        'layers': 'layers',
-        'obsm': 'obsm',
-        'obsp': 'obsp',
-    }
-    # filter out slots that aren't present in the input
-    kwargs = {k: v for k, v in kwargs.items() if k in [f.name for f in Path(input_file).iterdir()]}
-
-adata = read_anndata(
-    input_file,
-    **kwargs,
-    backed=backed,
-    dask=dask
-)
+adata = read_anndata(input_file, **kwargs)
 logging.info(adata.__str__())
 
-keep_indices = adata.obs_names.tolist()
+keep_indices = (x for x in adata.obs_names)
 remove_indices = []
 
-# explicit filter
 if 'remove_by_column' in params:
     logging.info('remove by columns...')
     ex_filters = params['remove_by_column']
     logging.info(pformat(ex_filters))
     for column, values in ex_filters.items():
-        logging.info(f'remove {values} in column="{column}"...')
+        logging.info(f'remove {values} from column="{column}"...')
         values = [str(v) for v in values]
         remove_indices.extend(
             adata.obs_names[adata.obs[column].astype(str).isin(values)]
         )
-        remove_indices = list(set(remove_indices))
 
-keep_indices = [i for i in keep_indices if i not in remove_indices]
-n_removed = adata.n_obs - len(keep_indices)
-logging.info(f'keep {len(keep_indices)} cells')
-logging.info(f'remove {n_removed} cells')
-
-logging.info('Save filtered...')
+logging.info('Add filtered column...')
+keep_indices = (i for i in keep_indices if i not in remove_indices)
 adata.obs['filtered'] = adata.obs_names.isin(keep_indices)
+value_counts = adata.obs['filtered'].value_counts()
+logging.info(value_counts)
 
-if subset:
+if subset and False in value_counts.index:
+    kwargs |= {
+        'X': 'X',
+        'layers': 'layers',
+        'raw': 'raw',
+        'obsm': 'obsm',
+        'obsp': 'obsp',
+    }
+    # filter out slots that aren't present in the input
+    kwargs = {k: v for k, v in kwargs.items() if k in [f.name for f in Path(input_file).iterdir()]}
+    
+    logging.info('Read all slots for subsetting...')
+    obs = adata.obs # save updated obs
+    adata = read_anndata(
+        input_file,
+        backed=backed,
+        dask=dask,
+        **{k: v for k, v in kwargs.items() if k != 'obs'},
+    )
+    adata.obs = obs # updated obs
+    
     logging.info('Subset data by filters...')
     adata = adata[adata.obs['filtered']].copy()
+    logging.info(adata.__str__())
 
 logging.info(f'Write to {output_file}...')
 write_zarr_linked(
