ebe3e0e7288af66f47e0c1d2ec52e12d857767ce
Michaela Mueller <michaela.muller@helmholtz-munich.de>
2024-04-03 08:24:12 -0700

    improve filtering performance with pandas series instead of lists

--- workflow/filter/scripts/filter.py
+++ workflow/filter/scripts/filter.py
@@ -4,6 +4,7 @@ Data filtering
 from pathlib import Path
 from pprint import pformat
 import numpy as np
+import pandas as pd
 import scanpy as sc
 from dask import array as da
 import logging
@@ -24,8 +25,7 @@ kwargs = {'obs': 'obs'}
 adata = read_anndata(input_file, **kwargs)
 logging.info(adata.__str__())
 
-keep_indices = (x for x in adata.obs_names)
-remove_indices = []
+mask = pd.Series(np.full(adata.n_obs, True, dtype=bool), index=adata.obs_names)
 
 if 'remove_by_column' in params:
     logging.info('remove by columns...')
@@ -34,13 +34,10 @@ if 'remove_by_column' in params:
     for column, values in ex_filters.items():
         logging.info(f'remove {values} from column="{column}"...')
         values = [str(v) for v in values]
-        remove_indices.extend(
-            adata.obs_names[adata.obs[column].astype(str).isin(values)]
-        )
+        mask = mask & ~adata.obs[column].astype(str).isin(values)
 
 logging.info('Add filtered column...')
-keep_indices = (i for i in keep_indices if i not in remove_indices)
-adata.obs['filtered'] = adata.obs_names.isin(keep_indices)
+adata.obs['filtered'] = mask
 value_counts = adata.obs['filtered'].value_counts()
 logging.info(value_counts)
 
