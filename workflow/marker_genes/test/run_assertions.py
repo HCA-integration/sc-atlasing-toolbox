cf5bf771dc8bf931ca812ff0566d29545c1c6052
Michaela Mueller <michaela.muller@helmholtz-munich.de>
2024-05-15 09:04:41 -0700

    implement proper test for marker genes

--- workflow/marker_genes/test/run_assertions.py
+++ workflow/marker_genes/test/run_assertions.py
@@ -1,76 +1,31 @@
 import glob
 import warnings
-from pprint import pprint
-import numpy as np
+from pprint import pformat
+import yaml
 from anndata.experimental import read_elem
 import zarr
 import logging
 logging.basicConfig(level=logging.INFO)
 
 
+config = yaml.safe_load(open('test/config.yaml', 'r'))
+
 # direct integration outputs
-outputs = glob.glob('test/out/integration/dataset~*/file_id~*/batch~*/method~*/adata.zarr')
-outputs = glob.glob('test/out/integration/dataset~*/file_id~*/batch~*/method~*--output_type~*.zarr')
+outputs = glob.glob('test/out/marker_genes/dataset~*/file_id~*.zarr')
 if len(outputs) == 0:
     warnings.warn('No integration outputs found')
 
-logging.info('Checking file outputs...')
-
 for file in outputs:
-    # logging.info(f'Checking {file}...')
+    dataset = [x for x in config['DATASETS'] if f'dataset~{x}/' in file][0]
+
+    logging.info(f'dataset: {dataset}')
+    logging.info(f'Checking {file}...')
     z = zarr.open(file)
+
+    dataset_config = config['DATASETS'][dataset]['marker_genes']
+    logging.info(pformat(dataset_config))
+    
     uns = read_elem(z["uns"])
-
-    try:
-        # Check Metadata
-        assert 'wildcards' in uns
-        assert 'integration_batch' in uns['wildcards']
-        assert 'integration_hyperparams' in uns['wildcards']
-        assert 'integration_label' in uns['wildcards']
-        assert 'integration_method' in uns['wildcards']
-        assert 'dataset' in uns
-        assert 'methods' in uns
-        assert 'integration' in uns
-        for key in ['method', 'label_key', 'batch_key', 'output_type']:
-            assert key in uns['integration']
-
-        # Unintegrated data
-        # adata_raw = raw.to_adata()
-        # assert 'X_pca' in adata_raw.obsm
-        # assert 'connectivities' in adata_raw.obsp
-        # assert 'distances' in adata_raw.obsp
-
-        # Output type specific outputs
-        output_types = uns['integration']['output_type']
-        output_types = [output_types] if isinstance(output_types, str) else output_types
-
-        if uns['integration']['method'] == 'scanorama':
-            assert len(output_types) == 2
-
-        for ot in output_types:
-            if ot not in ['knn', 'embed', 'full']:
-                raise ValueError(f'Invalid output type {ot}')
-
-        if 'knn' in output_types:
-            assert 'obsp' in z, f'No obsp for output type knn\nfile: {file}'
-            obsp = read_elem(z["obsp"])
-            assert 'connectivities' in obsp
-            assert 'distances' in obsp
-        if 'embed' in output_types:
-            assert 'obsm' in z, f'No obsm for output type embed\nfile: {file}'
-            obsm = read_elem(z["obsm"])
-
-            assert 'X_emb' in obsm
-        if 'full' in output_types:
-            assert 'X' in z, f'No X for output type full\nfile: {file}'
-            X = read_elem(z['X'])
-            assert X is not None
-            # assert 'corrected_counts' in layers
-            # check that counts are different from input
-            # if uns['integration']['method'] != 'unintegrated':
-                # assert adata.X.data.shape != adata_raw.X.data.shape or np.any(X.data != adata_raw.X.data)
-
-    except AssertionError as e:
-        print('AssertionError for:', file)
-        pprint(uns.keys())
-        raise e
+    for group in dataset_config['groups']:
+        key = f'marker_genes_group={group}'
+        assert key in uns, f'"{key}" not found in {file}\nuns: {uns.keys()}'
