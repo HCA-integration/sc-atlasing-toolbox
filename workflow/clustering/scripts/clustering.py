8a16efb7c036297e05c4b34a8b7bb4c280d3b7eb
Michaela Müller <51025211+mumichae@users.noreply.github.com>
2024-05-16 11:10:52 +0200

    Clustering fallback (#160)
    * add fallback to scanpy clustering functions if too many clusters are computed
    
    * update tests

--- workflow/clustering/scripts/clustering.py
+++ workflow/clustering/scripts/clustering.py
@@ -26,6 +26,7 @@ resolution = float(snakemake.wildcards.resolution)
 neighbors_key = snakemake.params.get('neighbors_key', 'neighbors')
 cluster_alg = snakemake.params.get('algorithm', 'louvain')
 overwrite = snakemake.params.get('overwrite', False)
+USE_GPU = False
 
 cluster_key = f'{cluster_alg}_{resolution}'
 
@@ -41,6 +42,7 @@ else:
         import subprocess
         assert subprocess.run('nvidia-smi', shell=True).returncode == 0
         from _rsc_clustering import leiden, louvain
+        USE_GPU = True
     except AssertionError:
         logging.info('No GPU found...')
         from scanpy.tools import leiden, louvain
@@ -52,6 +54,8 @@ else:
 
     logging.info(f'Read anndata file {input_file}...')
     adata = read_anndata(input_file, obs='obs', uns='uns', obsp='obsp')
+    # following observations from https://github.com/rapidsai/cugraph/issues/4072#issuecomment-2074822898
+    adata.obsp['connectivities'] = adata.obsp['connectivities'].astype('float64')
 
     # select neighbors
     logging.info(f'Select neighbors for "{neighbors_key}"...')
@@ -60,6 +64,18 @@ else:
     logging.info(f'{cluster_alg} clustering with resolution {resolution}...')
     cluster_func = cluster_alg_map.get(cluster_alg, KeyError(f'Unknown clustering algorithm: {cluster_alg}'))
     cluster_func(adata, resolution=resolution, key_added=cluster_key)
+    
+    if USE_GPU and adata.obs[cluster_key].nunique() > 200:
+        # fallback when too many clusters are computed (assuming this is a bug in the rapids implementation)
+        import scanpy as sc
+        
+        alt_cluster_alg_map = {
+            'louvain': sc.tl.louvain,
+            'leiden': sc.tl.leiden,
+        }
+        logging.info(f'Cluster {cluster_key} has more than 100 unique values. Falling back to scanpy implementation...')
+        cluster_func = alt_cluster_alg_map[cluster_alg]
+        cluster_func(adata, resolution=resolution, key_added=cluster_key)
 
 logging.info(f'Write {cluster_key} to {output_file}...')
 adata.obs[[cluster_key]].to_parquet(output_file)
