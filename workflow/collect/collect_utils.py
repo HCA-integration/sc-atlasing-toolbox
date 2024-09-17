c3d7406db5e564cd2f3b331f0ad9c5ba46191a78
Michaela Müller <51025211+mumichae@users.noreply.github.com>
2024-09-17 21:06:04 +0200

    Collection module (#191)
    * add module for collecting slots of multiple input files
    
    * check that just values of index are the same, not ordering
    
    * allow to customise obs index column per file
    
    * document module
    
    * add collect module to global workflow
    
    * link slots for zarr input (except for pandas dataframes)
    
    * use default memory factor for rerun
    
    * optimise reading
    
    * add test dataset

---
+++ workflow/collect/collect_utils.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+
+def get_same_columns(adatas):
+    _ad1 = next(iter(adatas.values()))
+    # collect obs columns that exist in all files
+    same_obs_columns = set(
+        _ad1.obs.columns.tolist()
+    ).intersection(
+        *[_ad.obs.columns.tolist() for _ad in adatas.values()]
+    )
+    # determine which columns do not have the same values across all files
+    obs_to_remove = []
+    for col in same_obs_columns:
+        same_across = all(
+            _ad1.obs[col].sort_values().equals(_ad.obs[col].sort_values())
+            for _ad in adatas.values()
+        )
+        if not same_across:
+            obs_to_remove.append(col)
+    # remove columns that are not the same across all files
+    return [
+        col for col in same_obs_columns
+        if col not in obs_to_remove
+    ]
+
+
+def merge_df(
+    df_current,
+    file_id,
+    df_previous,
+    same_columns,
+    sep='_',
+    obs_index_col=None,
+):
+    if obs_index_col is not None:
+        assert obs_index_col in df_current.columns, f'Index column "{obs_index_col}" not found for {file_id}\n{df_current}'
+        df_current = df_current.set_index(obs_index_col)
+    if df_previous is None:
+        return df_current.rename(
+            columns={
+                col: f'{col}{sep}{file_id}'
+                for col in df_current.columns.tolist()
+                if col not in same_columns
+            }
+        )
+    
+    idx_diff = df_current.index.difference(df_previous.index).sort_values()
+    n_diff = len(idx_diff)
+    assert n_diff == 0, \
+        f'Index must be the same\n {n_diff} differing indices: {idx_diff}\n' \
+        f'current index: {df_current.index.sort_values()} \nprevious index: {df_previous.index.sort_values()}'
+    unique_columns = [col for col in df_current.columns if col not in same_columns]
+    df_current = df_current[unique_columns].rename(
+        columns={col: f'{col}{sep}{file_id}' for col in unique_columns}
+    )
+    df = pd.concat([df_previous, df_current], axis=1)
+    return df
