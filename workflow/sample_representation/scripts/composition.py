36f268898b01e897fb81892059f7ae11e00420a8
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2024-09-23 15:46:01 +0200

    Add composition representation method

---
+++ workflow/sample_representation/scripts/composition.py
@@ -0,0 +1,48 @@
+import logging
+import warnings
+
+import patient_representation as pr
+import pandas as pd
+import scanpy as sc
+
+from utils.io import read_anndata
+
+warnings.simplefilter("ignore", UserWarning)
+logging.basicConfig(level=logging.INFO)
+
+sc.set_figure_params(dpi=100, frameon=False)
+input_zarr = snakemake.input.zarr
+output_zarr = snakemake.output.zarr
+sample_key = snakemake.params.get('sample_key')
+cell_type_key = snakemake.params.get('cell_type_key')
+use_rep = snakemake.params.get('use_rep')
+
+logging.info(f'Read "{input_zarr}"...')
+n_obs = read_anndata(input_zarr, obs='obs').n_obs
+dask = n_obs > 2e6
+adata = read_anndata(
+    input_zarr,
+    X='X',
+    obs='obs',
+    var='var',
+    obsm='obsm',
+    backed=dask,
+    dask=dask,
+    stride=int(n_obs / 5),
+)
+
+logging.info(f'Calculating composition representation for "{cell_type_key}"')
+representation_method = pr.tl.CellTypesComposition(sample_key=sample_key, cells_type_key=cell_type_key, layer=use_rep)
+representation_method.prepare_anndata(adata)
+distances = representation_method.calculate_distance_matrix(force=True, dist="euclidean")
+
+# Create empty dataframe to put sample names to obs_names and var_names
+samples_df = pd.DataFrame(index=representation_method.samples)
+
+output_adata = sc.AnnData(X=distances, var=samples_df, obs=samples_df)
+output_adata.obsm[f'{cell_type_key}_composition'] = representation_method.patient_representations
+
+logging.info(output_adata.__str__())
+
+logging.info(f'Write "{output_zarr}"...')
+output_adata.write_zarr(output_zarr)
