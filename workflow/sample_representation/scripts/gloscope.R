469b4a4ed39f81cd9986dd1e7122d03e0be3d54f
Michaela Mueller <michaela.mueller@helmholtz-munich.de>
2024-10-04 15:39:22 -0700

    add scITD

--- workflow/sample_representation/scripts/gloscope.R
+++ workflow/sample_representation/scripts/gloscope.R
@@ -2,10 +2,10 @@ options(show.error.locations = TRUE)
 
 # Load required libraries
 suppressPackageStartupMessages({
-  library(anndata)
+  library(data.table)
   library(GloScope)
   library(reticulate)
-  library(data.table)
+  library(anndata)
 })
 io <- import('utils.io')
 ad <- import('anndata')
@@ -17,6 +17,7 @@ use_rep <- snakemake@params$use_rep
 sample_key <- snakemake@params$sample_key
 k <- snakemake@params$k
 seed <- snakemake@params$seed
+threads <- snakemake@threads
 
 
 message("Read data...")
@@ -32,8 +33,7 @@ sample_ids <- adata$obs[[sample_key]] # TOOD: is this correct?
 obs_dt <- data.table(adata$obs)
 sample_dt <- obs_dt[, lapply(.SD, first), by = sample_key]
 
-message("Data dimensions:")
-message(paste(dim(embedding), collapse=', '))
+message(paste(c("Data dimensions:", paste(dim(embedding), collapse=', '))))
 message(paste("Number of IDs:", uniqueN(sample_ids)))
 
 message("Calculating distance matrix...")
@@ -43,20 +43,19 @@ dist_matrix <- gloscope(
     dens = "GMM",
     dist_mat = "KL",
     k = k,
-    BPPARAM = BiocParallel::SerialParam(RNGseed = seed)
+    BPPARAM = BiocParallel::MulticoreParam(workers = 4, RNGseed = seed)
 )
-message("Distance matrix dimensions:")
-message(paste(dim(dist_matrix), collapse=', '))
+message(paste(c("Distance matrix dimensions:", paste(dim(dist_matrix), collapse=', '))))
 
 # create new anndata object
 message("Creating new anndata object...")
 adata <- ad$AnnData(
-    X = adata$dist_matrix,
+    X = dist_matrix,
     obs = sample_dt,
     var = sample_dt,
 )
-
 print(adata)
+
 message(paste("Writing to", output_file, "..."))
 io$write_zarr_linked(
     adata,
---
+++ workflow/sample_representation/scripts/scitd.R
@@ -0,0 +1,161 @@
+options(show.error.locations = TRUE)
+
+tryCatch({
+ suppressPackageStartupMessages(library(scITD))
+  }, error = function(e) {
+    install.packages('scITD', repos = snakemake@params$cran_url)
+  }
+)
+
+# Load required libraries
+suppressPackageStartupMessages({
+  library(data.table)
+  library(scITD)
+  library(reticulate)
+  library(anndata)
+})
+io <- import('utils.io')
+ad <- import('anndata')
+
+
+run_scITD <- function(
+    data_container,
+    n_factors = 5,
+    n_gene_sets = 10,
+    tucker_type = 'regular',
+    rotation_type = 'hybrid'
+) {
+  
+  ranks = c(n_factors, n_gene_sets)
+  
+  data_container <- run_tucker_ica(
+    data_container,
+    ranks = ranks,
+    tucker_type = tucker_type,
+    rotation_type = rotation_type
+  )
+  
+  all_scores <- list()
+  all_loadings <- list()
+  
+  # Iterate over the factors to collect all the scores and loadings
+  for (i in 1:n_factors) {
+    f_data <- get_one_factor(data_container, factor_select=i)
+    f_scores <- f_data[[1]]
+    all_scores[[i]] <- f_scores
+    
+    f_loadings <- f_data[[2]]
+    colnames(f_loadings) <- paste0("Factor", i, "_", colnames(f_loadings))
+    all_loadings[[i]] <- f_loadings
+  }
+  
+  # Combine all scores into a single data frame
+  scores_table <- do.call(cbind, all_scores)
+  colnames(scores_table) <- paste0("Factor", 1:n_factors)
+  
+  loadings_table <- do.call(cbind, all_loadings)
+  
+  list(
+    data_container = data_container,
+    scores = scores_table,
+    loadings = loadings_table
+  )
+}
+
+
+input_file <- snakemake@input$zarr
+output_file <- snakemake@output$zarr
+
+use_rep <- snakemake@params$use_rep
+sample_key <- snakemake@params$sample_key
+cell_type_key <- snakemake@params$cell_type_key
+threads <- snakemake@params$threads
+seed <- snakemake@params$seed
+norm_method <- 'trim'
+scale_factor <- 1e4
+var_scale_power <- 2
+scale_var <- TRUE
+
+message("Read data...")
+adata <- io$read_anndata(
+    input_file,
+    X=use_rep,
+    obs='obs',
+    var='var',
+)
+rownames(adata$X) <- adata$obs_names
+colnames(adata$X) <- adata$var_names
+rownames(adata$obs) <- adata$obs_names
+adata$obs[['donors']] <- adata$obs[[sample_key]]
+adata$obs[['ctypes']] <- adata$obs[[cell_type_key]]
+
+message("Initializing parameters...")
+param_list <- initialize_params(
+    ctypes_use = as.character(unique(adata$obs[[cell_type_key]])),
+    ncores = threads,
+    rand_seed = seed
+)
+
+message("Create data container...")
+data_container <-  make_new_container(
+    count_data = t(adata$X),
+    meta_data = adata$obs,
+    # gn_convert = adata$var_names,
+    params = param_list,
+    label_donor_sex = FALSE
+  )
+
+message("Forming a tensor")
+data_container <- form_tensor(
+    data_container, 
+    donor_min_cells = 1,
+    norm_method = norm_method,
+    scale_factor = scale_factor,
+    vargenes_method = 'norm_var_pvals',
+    vargenes_thresh = 0.1,
+    scale_var = scale_var,
+    var_scale_power = var_scale_power
+)
+n_hvgs <- length(data_container[["all_vargenes"]])
+if (n_hvgs == 0) {
+    data_container[['all_vargenes']] <- rownames(adata$X)
+}
+message(paste("Number of HVGs:", length(data_container[["all_vargenes"]])))
+
+# message("Running analysis for rank determination")
+# data_container <- determine_ranks_tucker(
+#     data_container,
+#     max_ranks_test = c(10, 5),
+#     shuffle_level = 'cells', 
+#     num_iter = 10, 
+#     norm_method = norm_method,
+#     scale_factor = scale_factor,
+#     scale_var = scale_var,
+#     var_scale_power = var_scale_power
+# )
+
+results <- run_scITD(
+    data_container,
+    n_factors = 5,
+    n_gene_sets = 10,
+)
+pairwise_distances <- dist(results$scores, method = "euclidean")
+
+# create new anndata object
+message("Creating new anndata object...")
+obs_dt <- data.table(adata$obs)
+sample_dt <- obs_dt[, lapply(.SD, first), by = sample_key]
+adata <- ad$AnnData(
+    X = pairwise_distances,
+    obs = sample_dt,
+    var = sample_dt,
+)
+print(adata)
+
+message(paste("Writing to", output_file, "..."))
+io$write_zarr_linked(
+    adata,
+    in_dir=input_file,
+    out_dir=output_file,
+    files_to_keep=c('X', 'layers' ,'obs', 'var', 'obsm', 'varm', 'obsp', 'varp', 'uns'),
+)
