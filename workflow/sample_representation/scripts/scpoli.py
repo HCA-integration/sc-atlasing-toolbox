c46f2f38f90ab597193930fdf7603c75bec5063e
Michaela Mueller <michaela.mueller@helmholtz-munich.de>
2024-10-05 13:12:59 -0700

    fix scpoli to correctly identify unannotated cells

--- workflow/sample_representation/scripts/scpoli.py
+++ workflow/sample_representation/scripts/scpoli.py
@@ -32,12 +32,16 @@ adata = read_anndata(
     stride=int(n_obs / 5),
 )
 
+adata.X = adata.X.astype('int32')
+adata.obs[cell_type_key] = adata.obs[cell_type_key].astype(str).fillna('NA').astype('category')
+
 logging.info(f'Calculating scPoli representation for "{cell_type_key}", using cell features from "{use_rep}"')
 representation_method = pr.tl.SCPoli(
     sample_key=sample_key,
     cells_type_key=cell_type_key,
     layer='X',
-    n_epochs=n_epochs
+    n_epochs=n_epochs,
+    unknown_ct_names=['NA'],
 )
 representation_method.prepare_anndata(adata)
 distances = representation_method.calculate_distance_matrix(force=True)
