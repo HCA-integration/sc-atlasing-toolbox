31498302dde047c9ed7b9f75ddf0a6e3a784a355
Michaela Mueller <michaela.mueller@helmholtz-munich.de>
2024-11-21 08:49:04 -0800

    fix slotmap for only feature output as well as raw data reading

--- workflow/metrics/test/run_test.sh
+++ workflow/metrics/test/run_test.sh
@@ -5,6 +5,6 @@ WORKDIR=$(dirname $(dirname $0))
 cd $WORKDIR
 
 #--snakefile $WORKDIR/Snakefile
-snakemake --rerun-incomplete --configfile test/config.yaml --use-conda $@
+snakemake --rerun-triggers mtime params input code --rerun-incomplete --configfile test/config.yaml --use-conda $@
 
 conda run -n scanpy python test/run_assertions.py
