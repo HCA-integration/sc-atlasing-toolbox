809363e29bd8c01ebb9b83b563485b82cb317f6a
Michaela Mueller <michaela.mueller@helmholtz-munich.de>
2025-01-19 05:17:06 -0800

    fix covariate and gene set parsing

--- workflow/metrics/MetricsConfig.py
+++ workflow/metrics/MetricsConfig.py
@@ -1,9 +1,9 @@
+import numpy as np
 import pandas as pd
 from pprint import pformat
 
 from utils.ModuleConfig import ModuleConfig
 from utils.marker_genes import get_marker_gene_set
-from utils.misc import unique_dataframe
 
 
 class MetricsConfig(ModuleConfig):
@@ -17,20 +17,20 @@ class MetricsConfig(ModuleConfig):
         """
         super().__init__(**kwargs, write_output_files=False)
         
-        self.set_gene_sets()
-        self.set_covariates()
+        wildcards_df = self.parameters.wildcards_df
+        wildcards_df = self.set_gene_sets(wildcards_df)
+        wildcards_df = self.set_covariates(wildcards_df)
         
         self.update_parameters(
-            wildcards_df=unique_dataframe(self.parameters.wildcards_df),
+            wildcards_df=wildcards_df,
             wildcard_names=self.parameters.wildcard_names,
             **self.parameters.paramspace_kwargs
         )
 
 
-    def set_gene_sets(self, config_key='gene_sets'):
+    def set_gene_sets(self, wildcards_df, config_key='gene_set'):
         gene_set_map = dict()
-        wildcards_df = self.parameters.wildcards_df
-        
+
         for dataset in self.datasets:
             gene_sets = get_marker_gene_set(
                 self,
@@ -40,30 +40,36 @@ class MetricsConfig(ModuleConfig):
             )
             gene_set_map[dataset] = gene_sets
             
-            # convert into format for exploding
-            # gene_sets = [{key: value} for key, value in gene_sets.items()]
-            gene_sets = list(gene_sets.items())
-            
             # set gene sets only for metrics that use it
             dataset_mask = wildcards_df['dataset'] == dataset
-            use_config_mask = wildcards_df[f'use_{config_key}'] & dataset_mask
+            use_config_mask = dataset_mask & wildcards_df[f'use_{config_key}']
 
-            wildcards_df.loc[dataset_mask, config_key] = None  # Default to None
+            wildcards_df.loc[dataset_mask, config_key] = None
             wildcards_df.loc[use_config_mask, config_key] = pd.Series(
-                [gene_sets] * use_config_mask.sum(),
-                dtype=object
+                [list(gene_sets.keys())] * use_config_mask.sum(),
+                index=use_config_mask[use_config_mask].index,
+                dtype=object,
             )
 
-        # explode gene sets so that each metric setting has a single gene set
-        self.wildcards_df = wildcards_df.explode(config_key)
         self.gene_set_map = gene_set_map
-    
-    
+
+        # explode gene sets so that each metric setting has a single gene set
+        wildcards_df = wildcards_df.explode(config_key, ignore_index=True)
+        wildcards_df[config_key] = wildcards_df[config_key].fillna('None')
+
+        return wildcards_df
+
+
     def get_gene_sets(self, dataset, **kwargs):
         return self.gene_set_map[dataset]
 
 
-    def set_covariates(self, config_key='covariates'):
-        wildcards_df = self.wildcards_df
+    def set_covariates(self, wildcards_df, config_key='covariate'):
+        # covariates only used for metrics that require it
         wildcards_df.loc[~wildcards_df[f'use_{config_key}'], config_key] = None
-        self.wildcards_df = wildcards_df.explode(config_key)
+
+        # explode covariates so that each metric setting has a single covariate
+        wildcards_df = wildcards_df.explode(config_key, ignore_index=True)
+        wildcards_df[config_key] = wildcards_df[config_key].fillna('None')
+
+        return wildcards_df
