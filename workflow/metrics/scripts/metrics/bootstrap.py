3066bd6200a02a9c534555169a495eb69a828bdf
Michaela Mueller <michaela.mueller@helmholtz-munich.de>
2025-01-20 15:32:51 -0800

    use rapids_singlecell if possible for bootstrapping

--- workflow/metrics/scripts/metrics/bootstrap.py
+++ workflow/metrics/scripts/metrics/bootstrap.py
@@ -1,32 +1,37 @@
 import numpy as np
 import anndata as ad
 import scanpy as sc
+from .utils_pipeline.processing import sc as rsc
 
 
-def get_bootstrap_adata(adata: ad.AnnData, size: int = None):
+def get_bootstrap_adata(adata: ad.AnnData, size: int = None, use_rep='X_emb'):
     if size is None:
         size = int(0.9 * adata.n_obs)
     
-    adata = ad.AnnData(
-        obs=adata.obs,
-        obsp=adata.obsp,
-        obsm=adata.obsm,
-    )
-    
+    print(f'Subset to {size} cells...', flush=True)
     rand_indices = np.random.choice(adata.obs_names, size=size, replace=False)
     adata_bootstrap = adata[rand_indices].copy()
     
     # Recalculation is important as kNN graph is input for Moran's I
+    print('Recalculate neighbors from distances...', flush=True)
+    n_neighbors = adata.uns.get('neighbors', {}).get('params', {}).get('n_neighbors', 15)
     try:
         adata_bootstrap.obsm['distances'] = adata_bootstrap.obsp['distances']
         sc.pp.neighbors(
             adata_bootstrap,
             use_rep='distances',
             metric='precomputed',
-            transformer='sklearn'
+            transformer='sklearn',
+            n_neighbors=n_neighbors,
         )
-    except ValueError:
-        sc.pp.neighbors(adata_bootstrap, use_rep='X_emb')
+    except ValueError as e:
+        print(f'Error in recalculation of neighbors:\n{e}', flush=True)
+        print('Recalculate neighbors from {use_rep}...', flush=True)
+        rsc.pp.neighbors(
+            adata_bootstrap,
+            use_rep=use_rep,
+            n_neighbors=n_neighbors
+        )
     finally:
         return adata_bootstrap
 
