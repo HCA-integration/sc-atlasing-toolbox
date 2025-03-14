cb7d58cdc23df6cf4f980b83edd08d150816c459
Michaela Mueller <michaela.mueller@helmholtz-munich.de>
2025-03-14 11:04:56 -0700

    update celltypist

--- workflow/cell_type_prediction/scripts/celltypist.py
+++ workflow/cell_type_prediction/scripts/celltypist.py
@@ -3,10 +3,13 @@ import scanpy as sc
 import celltypist
 from celltypist import models
 
+from utils.io import read_anndata
+
 sc.settings.verbosity = 3
 sc.set_figure_params(frameon=False)
 
-input_h5ad = snakemake.input.h5ad
+
+input_file = snakemake.input.h5ad
 input_model = snakemake.input.model
 method = snakemake.wildcards.method
 model_name = snakemake.wildcards.model
@@ -14,7 +17,8 @@ label_key = snakemake.params.label_key
 output = snakemake.output.tsv
 output_png = snakemake.output.png
 
-adata = sc.read(input_h5ad, cache=True)
+print(f'Read file: {input_file}...', flush=True)
+adata = read_anndata(input_file, X='X', obs='obs', var='var')
 
 # assign and subset adata based on feature names instead on ensembl IDs
 if 'feature_name' in adata.var.columns:
@@ -25,19 +29,16 @@ adata = adata[:, non_ENS_genes_list].copy()
 # normalise and log1p the raw counts TODO: move to preprocessing module
 sc.pp.normalize_total(adata, target_sum=1e4)
 sc.pp.log1p(adata)
-sc.pp.highly_variable_genes(adata)
-sc.pp.pca(adata)
-sc.pp.neighbors(adata)
 
 # run celltypist
 # Select the model from the above list. If the `model` argument is not provided, will default to `Immune_All_Low.pkl`.
 model = models.Model.load(model=input_model)
 # The model summary information.
-print(model)
+print(model, flush=True)
 
 # Predict the identity of each input cell with the new model.
 predictions = celltypist.annotate(adata, model=model, majority_voting=True)
-print(predictions)
+print(predictions, flush=True)
 
 # plot predictions vs author labels
 plt.rcParams['figure.figsize'] = 10, 20  # TODO: make dependent on number of cell types
